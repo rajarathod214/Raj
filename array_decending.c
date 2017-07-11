aeffc47d7736c9a19f856b6c6994325c7805a93f
Rathod Raja <rajarathod214@yahoo.com>
2017-07-11 12:48:02 +0530

    Array asending and decending successfully added to master branch

---
+++ array_decending.c
@@ -0,0 +1,46 @@
+#include<stdio.h>
+
+
+int main()
+{
+	int array[50],i,j,size,temp;
+
+
+	printf("\n ENter the size of array:");
+	scanf("%d",&size);
+
+
+	printf("\n Enter the array elements:\n");
+	for(i=0;i<size;i++)
+	{
+		printf("array[%d]=",i);
+		scanf("%d",&array[i]);
+	}
+
+
+	for(i=0;i<size;i++)
+	{
+		for(j=i+1;j<size;j++)
+		{
+
+			if(array[i]<array[j])
+			{
+				temp=array[i];
+				array[i]=array[j];
+				array[j]=temp;
+			}
+		}	
+
+	}
+
+
+	printf("\n Desending order  elements of array:\n");
+	for(i=0;i<size;i++)
+	{
+		printf("array[%d]=%d\n",i,array[i]);
+
+	}
+
+return 0;
+
+}
